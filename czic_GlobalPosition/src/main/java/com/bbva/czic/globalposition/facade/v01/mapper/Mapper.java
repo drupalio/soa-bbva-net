9297ba3044946c881b31269d7050da6123415565
Julio Indriago <julio.indriago@entelgy.com>
2014-12-17 16:35:23 -0500

    Creación del mapeo de la lista de Dtos de productos internos a La lista de Productos externos.

--- czic_GlobalPosition/src/main/java/com/bbva/czic/globalposition/facade/v01/mapper/Mapper.java
+++ czic_GlobalPosition/src/main/java/com/bbva/czic/globalposition/facade/v01/mapper/Mapper.java
@@ -23,134 +23,145 @@ import com.bbva.czic.globalposition.business.dto.DTOIntEmail;
 import com.bbva.czic.globalposition.business.dto.DTOIntMovement;
 import com.bbva.czic.globalposition.business.dto.DTOIntEnumPhoneNumberType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Mapper {
 
-		
- public static EnumProductType enumProductTypeMap(DTOIntEnumProductType dtoIntEnumProductType) {
- 		return EnumProductType.valueOf(dtoIntEnumProductType.name());
- } 
 
-		
- public static DTOIntEnumProductType dtoIntEnumProductTypeMap(EnumProductType enumProductType) { 
- 		return DTOIntEnumProductType.valueOf(enumProductType.name());
- } 
+	public static EnumProductType enumProductTypeMap(DTOIntEnumProductType dtoIntEnumProductType) {
+		return EnumProductType.valueOf(dtoIntEnumProductType.name());
+	}
 
-		
- public static EnumFinancialStatusType enumFinancialStatusTypeMap(DTOIntEnumFinancialStatusType dtoIntEnumFinancialStatusType) { 
- 		return EnumFinancialStatusType.valueOf(dtoIntEnumFinancialStatusType.name());
- } 
 
-		
- public static DTOIntEnumFinancialStatusType dtoIntEnumFinancialStatusTypeMap(EnumFinancialStatusType enumFinancialStatusType) { 
- 		return DTOIntEnumFinancialStatusType.valueOf(enumFinancialStatusType.name());
- } 
+	public static DTOIntEnumProductType dtoIntEnumProductTypeMap(EnumProductType enumProductType) {
+		return DTOIntEnumProductType.valueOf(enumProductType.name());
+	}
 
-		
- public static Product productMap(DTOIntProduct dtoIntProduct) { 
- 		Product product = new Product(); 
- 		BeanUtils.copyProperties(dtoIntProduct, product); 
- 		return product; 
- } 
 
-		
- public static DTOIntProduct dtoIntProductMap(Product product) { 
- 		DTOIntProduct dtoIntProduct = new DTOIntProduct(); 
- 		BeanUtils.copyProperties(product, dtoIntProduct); 
- 		return dtoIntProduct; 
- } 
+	public static EnumFinancialStatusType enumFinancialStatusTypeMap(DTOIntEnumFinancialStatusType dtoIntEnumFinancialStatusType) {
+		return EnumFinancialStatusType.valueOf(dtoIntEnumFinancialStatusType.name());
+	}
 
-		
- public static Operation operationMap(DTOIntOperation dtoIntOperation) { 
- 		Operation operation = new Operation(); 
- 		BeanUtils.copyProperties(dtoIntOperation, operation); 
- 		return operation; 
- } 
 
-		
- public static DTOIntOperation dtoIntOperationMap(Operation operation) { 
- 		DTOIntOperation dtoIntOperation = new DTOIntOperation(); 
- 		BeanUtils.copyProperties(operation, dtoIntOperation); 
- 		return dtoIntOperation; 
- } 
+	public static DTOIntEnumFinancialStatusType dtoIntEnumFinancialStatusTypeMap(EnumFinancialStatusType enumFinancialStatusType) {
+		return DTOIntEnumFinancialStatusType.valueOf(enumFinancialStatusType.name());
+	}
 
-		
- public static EnumContactSourceType enumContactSourceTypeMap(DTOIntEnumContactSourceType dtoIntEnumContactSourceType) { 
- 		return EnumContactSourceType.valueOf(dtoIntEnumContactSourceType.name());
- } 
 
-		
- public static DTOIntEnumContactSourceType dtoIntEnumContactSourceTypeMap(EnumContactSourceType enumContactSourceType) { 
- 		return DTOIntEnumContactSourceType.valueOf(enumContactSourceType.name());
- } 
+	public static Product productMap(DTOIntProduct dtoIntProduct) {
+		Product product = new Product();
+		BeanUtils.copyProperties(dtoIntProduct, product);
+		return product;
+	}
 
-		
- public static PhoneNumber phoneNumberMap(DTOIntPhoneNumber dtoIntPhoneNumber) { 
- 		PhoneNumber phoneNumber = new PhoneNumber(); 
- 		BeanUtils.copyProperties(dtoIntPhoneNumber, phoneNumber); 
- 		return phoneNumber; 
- } 
 
-		
- public static DTOIntPhoneNumber dtoIntPhoneNumberMap(PhoneNumber phoneNumber) { 
- 		DTOIntPhoneNumber dtoIntPhoneNumber = new DTOIntPhoneNumber(); 
- 		BeanUtils.copyProperties(phoneNumber, dtoIntPhoneNumber); 
- 		return dtoIntPhoneNumber; 
- } 
+	public static DTOIntProduct dtoIntProductMap(Product product) {
+		DTOIntProduct dtoIntProduct = new DTOIntProduct();
+		BeanUtils.copyProperties(product, dtoIntProduct);
+		return dtoIntProduct;
+	}
 
-		
- public static ContactInformation contactInformationMap(DTOIntContactInformation dtoIntContactInformation) { 
- 		ContactInformation contactInformation = new ContactInformation(); 
- 		BeanUtils.copyProperties(dtoIntContactInformation, contactInformation); 
- 		return contactInformation; 
- } 
 
-		
- public static DTOIntContactInformation dtoIntContactInformationMap(ContactInformation contactInformation) { 
- 		DTOIntContactInformation dtoIntContactInformation = new DTOIntContactInformation(); 
- 		BeanUtils.copyProperties(contactInformation, dtoIntContactInformation); 
- 		return dtoIntContactInformation; 
- } 
+	public static Operation operationMap(DTOIntOperation dtoIntOperation) {
+		Operation operation = new Operation();
+		BeanUtils.copyProperties(dtoIntOperation, operation);
+		return operation;
+	}
 
-		
- public static Email emailMap(DTOIntEmail dtoIntEmail) { 
- 		Email email = new Email(); 
- 		BeanUtils.copyProperties(dtoIntEmail, email); 
- 		return email; 
- } 
 
-		
- public static DTOIntEmail dtoIntEmailMap(Email email) { 
- 		DTOIntEmail dtoIntEmail = new DTOIntEmail(); 
- 		BeanUtils.copyProperties(email, dtoIntEmail); 
- 		return dtoIntEmail; 
- } 
+	public static DTOIntOperation dtoIntOperationMap(Operation operation) {
+		DTOIntOperation dtoIntOperation = new DTOIntOperation();
+		BeanUtils.copyProperties(operation, dtoIntOperation);
+		return dtoIntOperation;
+	}
 
-		
- public static Movement movementMap(DTOIntMovement dtoIntMovement) { 
- 		Movement movement = new Movement(); 
- 		BeanUtils.copyProperties(dtoIntMovement, movement); 
- 		return movement; 
- } 
 
-		
- public static DTOIntMovement dtoIntMovementMap(Movement movement) { 
- 		DTOIntMovement dtoIntMovement = new DTOIntMovement(); 
- 		BeanUtils.copyProperties(movement, dtoIntMovement); 
- 		return dtoIntMovement; 
- } 
+	public static EnumContactSourceType enumContactSourceTypeMap(DTOIntEnumContactSourceType dtoIntEnumContactSourceType) {
+		return EnumContactSourceType.valueOf(dtoIntEnumContactSourceType.name());
+	}
 
-		
- public static EnumPhoneNumberType enumPhoneNumberTypeMap(DTOIntEnumPhoneNumberType dtoIntEnumPhoneNumberType) { 
- 		return EnumPhoneNumberType.valueOf(dtoIntEnumPhoneNumberType.name());
- } 
 
-		
- public static DTOIntEnumPhoneNumberType dtoIntEnumPhoneNumberTypeMap(EnumPhoneNumberType enumPhoneNumberType) { 
- 		return DTOIntEnumPhoneNumberType.valueOf(enumPhoneNumberType.name());
- } 
+	public static DTOIntEnumContactSourceType dtoIntEnumContactSourceTypeMap(EnumContactSourceType enumContactSourceType) {
+		return DTOIntEnumContactSourceType.valueOf(enumContactSourceType.name());
+	}
 
-	
-	
+
+	public static PhoneNumber phoneNumberMap(DTOIntPhoneNumber dtoIntPhoneNumber) {
+		PhoneNumber phoneNumber = new PhoneNumber();
+		BeanUtils.copyProperties(dtoIntPhoneNumber, phoneNumber);
+		return phoneNumber;
+	}
+
+
+	public static DTOIntPhoneNumber dtoIntPhoneNumberMap(PhoneNumber phoneNumber) {
+		DTOIntPhoneNumber dtoIntPhoneNumber = new DTOIntPhoneNumber();
+		BeanUtils.copyProperties(phoneNumber, dtoIntPhoneNumber);
+		return dtoIntPhoneNumber;
+	}
+
+
+	public static ContactInformation contactInformationMap(DTOIntContactInformation dtoIntContactInformation) {
+		ContactInformation contactInformation = new ContactInformation();
+		BeanUtils.copyProperties(dtoIntContactInformation, contactInformation);
+		return contactInformation;
+	}
+
+
+	public static DTOIntContactInformation dtoIntContactInformationMap(ContactInformation contactInformation) {
+		DTOIntContactInformation dtoIntContactInformation = new DTOIntContactInformation();
+		BeanUtils.copyProperties(contactInformation, dtoIntContactInformation);
+		return dtoIntContactInformation;
+	}
+
+
+	public static Email emailMap(DTOIntEmail dtoIntEmail) {
+		Email email = new Email();
+		BeanUtils.copyProperties(dtoIntEmail, email);
+		return email;
+	}
+
+
+	public static DTOIntEmail dtoIntEmailMap(Email email) {
+		DTOIntEmail dtoIntEmail = new DTOIntEmail();
+		BeanUtils.copyProperties(email, dtoIntEmail);
+		return dtoIntEmail;
+	}
+
+
+	public static Movement movementMap(DTOIntMovement dtoIntMovement) {
+		Movement movement = new Movement();
+		BeanUtils.copyProperties(dtoIntMovement, movement);
+		return movement;
+	}
+
+
+	public static DTOIntMovement dtoIntMovementMap(Movement movement) {
+		DTOIntMovement dtoIntMovement = new DTOIntMovement();
+		BeanUtils.copyProperties(movement, dtoIntMovement);
+		return dtoIntMovement;
+	}
+
+
+	public static EnumPhoneNumberType enumPhoneNumberTypeMap(DTOIntEnumPhoneNumberType dtoIntEnumPhoneNumberType) {
+		return EnumPhoneNumberType.valueOf(dtoIntEnumPhoneNumberType.name());
+	}
+
+
+	public static DTOIntEnumPhoneNumberType dtoIntEnumPhoneNumberTypeMap(EnumPhoneNumberType enumPhoneNumberType) {
+		return DTOIntEnumPhoneNumberType.valueOf(enumPhoneNumberType.name());
+	}
+
+	public static List<Product> productListMap(List<DTOIntProduct> intProducts) {
+
+		List<Product> products = new ArrayList<Product>();
+
+		for(DTOIntProduct intProduct: intProducts) {
+			products.add(productMap(intProduct));
+		}
+		return products;
+	}
+
 }
 
