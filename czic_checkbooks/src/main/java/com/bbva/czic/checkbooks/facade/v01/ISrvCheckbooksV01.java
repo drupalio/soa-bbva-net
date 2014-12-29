c1319beea6510b6a516fda2a33a8a7e4378c1c94
Julio Indriago <julio.indriago@entelgy.com>
2014-12-29 18:37:31 -0500

    Ajustes de los URL de checkbook

--- czic_checkbooks/src/main/java/com/bbva/czic/checkbooks/facade/v01/ISrvCheckbooksV01.java
+++ czic_checkbooks/src/main/java/com/bbva/czic/checkbooks/facade/v01/ISrvCheckbooksV01.java
@@ -1,13 +1,14 @@
 package com.bbva.czic.checkbooks.facade.v01;
 
 import com.bbva.czic.dto.net.Check;
+import com.bbva.czic.dto.net.Checkbook;
 
 import javax.ws.rs.core.Response;
 import java.util.List;
 
 public interface ISrvCheckbooksV01 {
 
-    List<Check> getChecks(String idCheckbook);
+    Check getChecks(String idCheck, String filter, String fields, String expands, String sort);
 
-    Response getCheckbooks(String filter, String fields, String expands, String sort);
+    Checkbook getCheckbooks(String checkbookId, String filter, String fields, String expands, String sort);
 }
