d8426b98a39fb0316fcf8971fcdb4c99c550a585
Julio Indriago <julio.indriago@entelgy.com>
2015-01-06 17:53:04 -0500

    Agregando Transacciones para Visibility y Operability.

---
+++ czic_GlobalPosition/src/main/java/com/bbva/czic/globalposition/dao/tx/TxUpdateProductOperability.java
@@ -0,0 +1,10 @@
+package com.bbva.czic.globalposition.dao.tx;
+
+/**
+ * Created by Administrador on 06/01/2015.
+ */
+public class TxUpdateProductOperability {
+
+    
+
+}
---
+++ czic_GlobalPosition/src/main/java/com/bbva/czic/globalposition/dao/tx/TxUpdateProductVisibility.java
@@ -0,0 +1,7 @@
+package com.bbva.czic.globalposition.dao.tx;
+
+/**
+ * Created by Administrador on 06/01/2015.
+ */
+public class TxUpdateProductVisibility {
+}
