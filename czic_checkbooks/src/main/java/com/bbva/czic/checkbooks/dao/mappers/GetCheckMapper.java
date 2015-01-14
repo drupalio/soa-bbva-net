d1ecf2193d0fc74f522fb0a4be3ed06e2fc73053
Julio Indriago <julio.indriago@entelgy.com>
2015-01-14 16:14:28 -0500

    Cambio del currency para que venga de un Enum.

--- czic_checkbooks/src/main/java/com/bbva/czic/checkbooks/dao/mappers/GetCheckMapper.java
+++ czic_checkbooks/src/main/java/com/bbva/czic/checkbooks/dao/mappers/GetCheckMapper.java
@@ -4,18 +4,17 @@ import com.bbva.czic.checkbooks.business.dto.DTOIntCheck;
 import com.bbva.czic.checkbooks.dao.model.ozny.FormatoOZECNYE0;
 import com.bbva.czic.checkbooks.dao.model.ozny.FormatoOZECNYS0;
 import com.bbva.czic.dto.net.EnumCheckStatus;
+import com.bbva.czic.routine.commons.rm.utils.CurrencyEnum;
 import com.bbva.czic.routine.commons.rm.utils.tx.ISimpleTransactionMapper;
 import com.bbva.jee.arq.spring.core.servicing.utils.Money;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import sun.util.resources.CurrencyNames_es_CO;
 
 import java.util.Currency;
 
 @Component(value = "tx-get-checks-mapper")
 public class GetCheckMapper implements ISimpleTransactionMapper<DTOIntCheck, FormatoOZECNYE0, DTOIntCheck, FormatoOZECNYS0> {
-
-    @Value("${locale.default.currency.iso}")
-    private String CURRENCY;
 
     @Override
     public FormatoOZECNYE0 mapToInner(DTOIntCheck dtoIn) {
@@ -33,7 +32,7 @@ public class GetCheckMapper implements ISimpleTransactionMapper<DTOIntCheck, For
 
         intCheck.setId(outFormat.getNumcheq());
         intCheck.setIssueDate(outFormat.getFechemi());
-        intCheck.setValue(new Money(Currency.getInstance(CURRENCY), outFormat.getValcheq()));
+        intCheck.setValue(new Money(Currency.getInstance(CurrencyEnum.COP.toString()), outFormat.getValcheq()));
         intCheck.setStatus(EnumCheckStatus.valueOf(outFormat.getEstcheq()));
         intCheck.setModifiedDate(outFormat.getFechmod());
 
