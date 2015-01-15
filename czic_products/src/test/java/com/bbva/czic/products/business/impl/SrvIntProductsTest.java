3b5575f7339d9b48227e3e4286c4493319177d1e
andrescrojas <ing-andresrojas@hotmail.com>
2015-01-15 11:18:24 -0500

    ajuste de Cards mapper, cambio de parametria de tipo String a tipo DTOIntProduct

--- czic_products/src/test/java/com/bbva/czic/products/business/impl/SrvIntProductsTest.java
+++ czic_products/src/test/java/com/bbva/czic/products/business/impl/SrvIntProductsTest.java
@@ -1,9 +1,12 @@
 package com.bbva.czic.products.business.impl;
 
 
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.when;
-
+import com.bbva.czic.products.business.ISrvIntProducts;
+import com.bbva.czic.products.business.dto.DTOIntConditions;
+import com.bbva.czic.products.business.dto.DTOIntProduct;
+import com.bbva.czic.products.dao.IProductsDAO;
+import com.bbva.jee.arq.spring.core.servicing.test.BusinessServiceTestContextLoader;
+import com.bbva.jee.arq.spring.core.servicing.test.MockInvocationContextTestExecutionListener;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,43 +19,40 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-import com.bbva.czic.products.business.ISrvIntProducts;
-import com.bbva.czic.products.business.dto.DTOIntConditions;
-import com.bbva.czic.products.dao.IProductsDAO;
-import com.bbva.jee.arq.spring.core.servicing.test.BusinessServiceTestContextLoader;
-import com.bbva.jee.arq.spring.core.servicing.test.MockInvocationContextTestExecutionListener;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(loader = BusinessServiceTestContextLoader.class, locations = {"classpath*:/META-INF/spring/applicationContext-*.xml", "classpath:/META-INF/spring/business-service.xml", "classpath:/META-INF/spring/business-service-test.xml"})
 @TestExecutionListeners(listeners = {MockInvocationContextTestExecutionListener.class, DependencyInjectionTestExecutionListener.class})
 public class SrvIntProductsTest {
-	
+
 	@Mock
 	IProductsDAO productsDAO;
 
 	@InjectMocks
 	@Autowired
 	ISrvIntProducts srv;
-	
+
 	@Before
 	public void init() {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	@Test()
 	public void testGetConditionsSucces() {
 		DTOIntConditions initialResult =null;
 
 		// setUp - expectation
-		when(productsDAO.getConditions("1234567890"))
-				.thenReturn(new DTOIntConditions());
+		DTOIntProduct dtoIntProduct = new DTOIntProduct();
+		when(productsDAO.getConditions(dtoIntProduct)).thenReturn(new DTOIntConditions());
 
-		initialResult= srv.getConditions("1234567890");
-		
+		initialResult= srv.getConditions(dtoIntProduct);
+
 		assertNotNull(initialResult);
 	}
-	
-	
+
+
 }
 
