4f65c32b97552b0163e7fa2324eb866cb957305e
mpacheco <mario.pacheco@entelgy.com>
2015-01-24 19:39:51 -0500

    Ajustes en mock y Test de Integración

--- czic_customers/src/main/java/com/bbva/czic/customers/dao/model/oznq/TransaccionOznq.java
+++ czic_customers/src/main/java/com/bbva/czic/customers/dao/model/oznq/TransaccionOznq.java
@@ -1,11 +1,12 @@
 package com.bbva.czic.customers.dao.model.oznq;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.bbva.jee.arq.spring.core.host.ExcepcionTransaccion;
-import com.bbva.jee.arq.spring.core.host.ServicioTransacciones;
 import com.bbva.jee.arq.spring.core.host.InvocadorTransaccion;
+import com.bbva.jee.arq.spring.core.host.ServicioTransacciones;
 
 /**
  * Invocador de la transacci&oacute;n <code>OZNQ</code>
@@ -13,22 +14,26 @@ import com.bbva.jee.arq.spring.core.host.InvocadorTransaccion;
  * @see PeticionTransaccionOznq
  * @see RespuestaTransaccionOznq
  */
-@Component
-public class TransaccionOznq implements InvocadorTransaccion<PeticionTransaccionOznq,RespuestaTransaccionOznq> {
-	
+@Profile(value = "prod")
+@Component(value = "transaccionOznq")
+public class TransaccionOznq implements InvocadorTransaccion<PeticionTransaccionOznq, RespuestaTransaccionOznq> {
+
 	@Autowired
 	private ServicioTransacciones servicioTransacciones;
-	
+
 	@Override
 	public RespuestaTransaccionOznq invocar(PeticionTransaccionOznq transaccion) throws ExcepcionTransaccion {
-		return servicioTransacciones.invocar(PeticionTransaccionOznq.class, RespuestaTransaccionOznq.class, transaccion);
+		return servicioTransacciones
+				.invocar(PeticionTransaccionOznq.class, RespuestaTransaccionOznq.class, transaccion);
 	}
-	
+
 	@Override
 	public RespuestaTransaccionOznq invocarCache(PeticionTransaccionOznq transaccion) throws ExcepcionTransaccion {
-		return servicioTransacciones.invocar(PeticionTransaccionOznq.class, RespuestaTransaccionOznq.class, transaccion);
+		return servicioTransacciones
+				.invocar(PeticionTransaccionOznq.class, RespuestaTransaccionOznq.class, transaccion);
 	}
-	
+
 	@Override
-	public void vaciarCache() {}	
+	public void vaciarCache() {
+	}
 }
