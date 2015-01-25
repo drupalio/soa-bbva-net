057edf831f9983c6c411eb4a10df424695ae2fbc
Nerly <nerly.aguilera@entelgy.com>
2015-01-24 20:39:05 -0500

    Se agrega Mock de Cards

--- czic_GlobalPosition/src/main/java/com/bbva/czic/globalposition/facade/v01/impl/SrvGlobalPositionV01.java
+++ czic_GlobalPosition/src/main/java/com/bbva/czic/globalposition/facade/v01/impl/SrvGlobalPositionV01.java
@@ -1,9 +1,30 @@
 package com.bbva.czic.globalposition.facade.v01.impl;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.apache.cxf.jaxrs.model.wadl.ElementClass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
 import com.bbva.czic.dto.net.Product;
 import com.bbva.czic.globalposition.business.ISrvIntGlobalPosition;
-import com.bbva.czic.globalposition.business.dto.DTOIntProductFilter;
 import com.bbva.czic.globalposition.business.dto.DTOIntProduct;
+import com.bbva.czic.globalposition.business.dto.DTOIntProductFilter;
 import com.bbva.czic.globalposition.facade.v01.ISrvGlobalPositionV01;
 import com.bbva.czic.globalposition.facade.v01.mapper.IGlobalPositionMapper;
 import com.bbva.czic.globalposition.facade.v01.utils.converters.IFilterConverter;
@@ -14,30 +35,23 @@ import com.bbva.jee.arq.spring.core.servicing.annotations.SMC;
 import com.bbva.jee.arq.spring.core.servicing.annotations.SN;
 import com.bbva.jee.arq.spring.core.servicing.annotations.VN;
 import com.bbva.jee.arq.spring.core.servicing.gce.BusinessServiceException;
-import com.wordnik.swagger.annotations.*;
-import org.apache.cxf.jaxrs.model.wadl.ElementClass;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
-
-import javax.annotation.Resource;
-import javax.ws.rs.*;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
-import java.util.List;
-
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
 
 @Path("/V01")
-@SN(registryID="SNCO1400002",logicalID="GlobalPosition")
-@VN(vnn="V01")
-@Api(value="/GlobalPosition/V01",description="SN GlobalPosition")
-@Produces({ MediaType.APPLICATION_JSON})
+@SN(registryID = "SNCO1400002", logicalID = "GlobalPosition")
+@VN(vnn = "V01")
+@Api(value = "/GlobalPosition/V01", description = "SN GlobalPosition")
+@Produces({ MediaType.APPLICATION_JSON })
 @Service
-public class SrvGlobalPositionV01 implements ISrvGlobalPositionV01, com.bbva.jee.arq.spring.core.servicing.utils.ContextAware {
+public class SrvGlobalPositionV01 implements ISrvGlobalPositionV01,
+		com.bbva.jee.arq.spring.core.servicing.utils.ContextAware {
 
-	private static I18nLog log = I18nLogFactory.getLogI18n(SrvGlobalPositionV01.class,"META-INF/spring/i18n/log/mensajesLog");
+	private static I18nLog log = I18nLogFactory.getLogI18n(SrvGlobalPositionV01.class,
+			"META-INF/spring/i18n/log/mensajesLog");
 
 	public HttpHeaders httpHeaders;
 
@@ -51,32 +65,29 @@ public class SrvGlobalPositionV01 implements ISrvGlobalPositionV01, com.bbva.jee
 
 	@Override
 	public void setUriInfo(UriInfo ui) {
-		this.uriInfo = ui;		
+		this.uriInfo = ui;
 	}
 
 	@Override
 	public void setHttpHeaders(HttpHeaders httpHeaders) {
 		this.httpHeaders = httpHeaders;
 	}
-	
+
 	@Autowired
 	ISrvIntGlobalPosition srvIntGlobalPosition;
 
-	
-	@ApiOperation(value="Gets all the users products", notes="List of products that COULD be filtered by product type.", response=List.class)
-	@ApiResponses(value = {
-		@ApiResponse(code = -1, message = "wrongParameters"),
-		@ApiResponse(code = -1, message = "noData"),
-		@ApiResponse(code = 200, message = "Found Sucessfully", response=List.class),
-		@ApiResponse(code = 500, message = "Technical Error"),
-		@ApiResponse(code = 400, message = "wrongParameters"),
-		@ApiResponse(code = 409, message = "noData")
-	})
+	@Override
+	@ApiOperation(value = "Gets all the users products", notes = "List of products that COULD be filtered by product type.", response = List.class)
+	@ApiResponses(value = { @ApiResponse(code = -1, message = "wrongParameters"),
+			@ApiResponse(code = -1, message = "noData"),
+			@ApiResponse(code = 200, message = "Found Sucessfully", response = List.class),
+			@ApiResponse(code = 500, message = "Technical Error"),
+			@ApiResponse(code = 400, message = "wrongParameters"), @ApiResponse(code = 409, message = "noData") })
 	@GET
 	@Path("/customers/{customerId}/products")
-	@Produces({MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_JSON })
 	@ElementClass(response = List.class)
-	@SMC(registryID="SMCCO1400003",logicalID="getExtractGlobalBalance")
+	@SMC(registryID = "SMCCO1400003", logicalID = "getExtractGlobalBalance")
 	public List<Product> getExtractGlobalBalance(
 			@ApiParam(value = "Customer identifier") @PathParam("customerId") String customerId,
 			@ApiParam(value = "filter param") @DefaultValue("null") @QueryParam("$filter") String filter) {
@@ -95,17 +106,15 @@ public class SrvGlobalPositionV01 implements ISrvGlobalPositionV01, com.bbva.jee
 	}
 
 	@Override
-	@ApiOperation(value="Update the product.", notes="Update the product partially", response=Response.class)
-	@ApiResponses(value = {
-		@ApiResponse(code = -1, message = "wrongParameters"),
-		@ApiResponse(code = 200, message = "Modified Sucessfully", response=Response.class),
-		@ApiResponse(code = 500, message = "Technical Error"),
-		@ApiResponse(code = 400, message = "wrongParameters")
-	})
+	@ApiOperation(value = "Update the product.", notes = "Update the product partially", response = Response.class)
+	@ApiResponses(value = { @ApiResponse(code = -1, message = "wrongParameters"),
+			@ApiResponse(code = 200, message = "Modified Sucessfully", response = Response.class),
+			@ApiResponse(code = 500, message = "Technical Error"),
+			@ApiResponse(code = 400, message = "wrongParameters") })
 	@PUT
 	@Path("/{idProduct}/setProductVisibility")
 	@Consumes({ MediaType.APPLICATION_JSON })
-	@SMC(registryID="SMCCO1400004",logicalID="updateProductVisibility")
+	@SMC(registryID = "SMCCO1400004", logicalID = "updateProductVisibility")
 	public Response updateProductVisibility(
 			@ApiParam(value = "Product identifier") @PathParam("idProduct") String idProduct,
 			@ApiParam(value = "Product information") Product infoProduct) {
@@ -126,17 +135,15 @@ public class SrvGlobalPositionV01 implements ISrvGlobalPositionV01, com.bbva.jee
 	}
 
 	@Override
-	@ApiOperation(value="Update the product.", notes="Update the product partially", response=Response.class)
-	@ApiResponses(value = {
-			@ApiResponse(code = -1, message = "wrongParameters"),
-			@ApiResponse(code = 200, message = "Modified Sucessfully", response=Response.class),
+	@ApiOperation(value = "Update the product.", notes = "Update the product partially", response = Response.class)
+	@ApiResponses(value = { @ApiResponse(code = -1, message = "wrongParameters"),
+			@ApiResponse(code = 200, message = "Modified Sucessfully", response = Response.class),
 			@ApiResponse(code = 500, message = "Technical Error"),
-			@ApiResponse(code = 400, message = "wrongParameters")
-	})
+			@ApiResponse(code = 400, message = "wrongParameters") })
 	@PUT
 	@Path("/{idProduct}/setProductOperability")
 	@Consumes({ MediaType.APPLICATION_JSON })
-	@SMC(registryID="SMCCO1400005",logicalID="updateProductOperability")
+	@SMC(registryID = "SMCCO1400005", logicalID = "updateProductOperability")
 	public Response updateProductOperability(
 			@ApiParam(value = "Product identifier") @PathParam("idProduct") String idProduct,
 			@ApiParam(value = "Product information") Product infoProduct) {
