ff22cf0f1ff16124c069fc606ccc33379db39366
mpacheco <mario.pacheco@entelgy.com>
2015-01-30 17:58:22 -0500

    ajuste a validaciones customerId->null, customerId-> min, max

--- czic_customers/src/main/java/com/bbva/czic/customers/facade/v01/impl/SrvCustomersV01.java
+++ czic_customers/src/main/java/com/bbva/czic/customers/facade/v01/impl/SrvCustomersV01.java
@@ -36,6 +36,7 @@ import com.bbva.czic.dto.net.CustomerOperation;
 import com.bbva.czic.routine.commons.rm.utils.errors.EnumError;
 import com.bbva.czic.routine.commons.rm.utils.fiql.FiqlType;
 import com.bbva.czic.routine.commons.rm.utils.validator.impl.FiqlValidator;
+import com.bbva.czic.routine.commons.rm.utils.validator.impl.StringValidator;
 import com.bbva.jee.arq.spring.core.log.I18nLog;
 import com.bbva.jee.arq.spring.core.log.I18nLogFactory;
 import com.bbva.jee.arq.spring.core.servicing.annotations.SMC;
@@ -132,6 +133,9 @@ public class SrvCustomersV01 implements ISrvCustomersV01, com.bbva.jee.arq.sprin
 
 		// 1. Validate filter FIQL
 		new FiqlValidator(filter).hasGeAndLe("month").validateIfExist();
+
+		// 1.b Validate customerId not is Null
+		new StringValidator().notIsNull(customerId).isNumericText(customerId).validate();
 
 		// 2. Mapping to DTOIntFilter
 		final DTOIntAccMovementsResumesFilter filterCustomerResumes = customerMapper.getDTOIntMovementResumesFilter(
