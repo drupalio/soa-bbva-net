3cfdf53554f9223a4a71faf22bc43e9acbf9dc79
David Leonardo Bernal <leo.bernal1946@gmail.com>
2015-01-30 11:34:52 -0500

    Implementacion metodo addChannel encustomers

---
+++ czic_customers/src/main/java/com/bbva/czic/customers/business/dto/DTOIntAddChannel.java
@@ -0,0 +1,42 @@
+package com.bbva.czic.customers.business.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by Entelgy on 30/01/2015.
+ */
+public class DTOIntAddChannel {
+
+    @NotNull
+
+    @Length(min = 8, max = 8)
+    private String customerId;
+
+    @NotNull
+
+    @Length(min = 4, max = 4)
+    private String channelId;
+
+    public DTOIntAddChannel(String customerId, String channelId){
+        this.customerId = customerId;
+        this.channelId = channelId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(String channelId) {
+        this.channelId = channelId;
+    }
+}
---
+++ czic_customers/src/main/java/com/bbva/czic/customers/dao/model/oznw/RespuestaTransaccionOznw.java
@@ -0,0 +1,55 @@
+package com.bbva.czic.customers.dao.model.oznw;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.serializable.RooSerializable;
+import org.springframework.roo.addon.tostring.RooToString;
+
+import com.bbva.jee.arq.spring.core.host.Cabecera;
+import com.bbva.jee.arq.spring.core.host.Cuerpo;
+import com.bbva.jee.arq.spring.core.host.CuerpoMultiparte;
+import com.bbva.jee.arq.spring.core.host.NombreCabecera;
+import com.bbva.jee.arq.spring.core.host.RespuestaTransaccion;
+import com.bbva.jee.arq.spring.core.host.Multiformato;
+import com.bbva.jee.arq.spring.core.host.MensajeMultiparte;
+
+/**
+ * Bean de respuesta para la transacci&oacute;n <code>OZNW</code>
+ * 
+ * @see PeticionTransaccionOznw
+ * 
+ * @author Arquitectura Spring BBVA
+ */
+@RespuestaTransaccion
+@Multiformato(formatos = {})
+@RooJavaBean
+@RooToString
+@RooSerializable
+public class RespuestaTransaccionOznw implements MensajeMultiparte {
+	
+	/**
+	 * <p>Cabecera <code>serviceResponse</code></p>
+	 */
+	@Cabecera(nombre=NombreCabecera.CODIGO_RETORNO)
+	private String codigoRetorno;
+	
+	/**
+	 * <p>Cabecera <code>processControl</code></p>
+	 */
+	@Cabecera(nombre=NombreCabecera.CODIGO_CONTROL)
+	private String codigoControl;
+	
+	/**
+	 * <p>Cuerpo del mensaje de respuesta multiparte</p>
+	 */
+	@Cuerpo
+	private CuerpoMultiparte cuerpo = new CuerpoMultiparte();
+	
+	/**
+	 * <p>Permite obtener el cuerpo del mensaje de petici&oacute;n multiparte</p>
+	 */
+	@Override
+	public CuerpoMultiparte getCuerpo() {
+		return cuerpo;
+	}
+	
+}
