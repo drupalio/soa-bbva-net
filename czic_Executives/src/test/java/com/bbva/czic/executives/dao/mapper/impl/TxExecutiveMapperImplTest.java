74b40c396c2e0df65fdca78017ae4207db081ae8
davidtafur <dtafur60@gmail.com>
2015-02-09 09:03:09 -0500

    Test de executives

--- czic_Executives/src/test/java/com/bbva/czic/executives/dao/mapper/impl/TxExecutiveMapperImplTest.java
+++ czic_Executives/src/test/java/com/bbva/czic/executives/dao/mapper/impl/TxExecutiveMapperImplTest.java
@@ -3,33 +3,65 @@ package com.bbva.czic.executives.dao.mapper.impl;
 import com.bbva.czic.dto.net.Executive;
 import com.bbva.czic.dto.net.Office;
 import com.bbva.czic.executives.business.dto.DTOIntExecutive;
+import com.bbva.czic.executives.business.dto.DTOIntExecutivesFilter;
+import com.bbva.czic.executives.dao.mapper.TxExecutiveMapper;
 import com.bbva.czic.executives.dao.mapper.impl.TxExecutiveMapperImpl;
+import com.bbva.czic.executives.dao.model.oznr.FormatoOZECNRE0;
+import com.bbva.czic.executives.dao.model.oznr.FormatoOZECNRS0;
+import com.bbva.czic.executives.facade.v01.mapper.IExecutivesMapper;
+import com.bbva.czic.executives.facade.v01.mapper.impl.ExecutivesMapper;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.Assert.assertEquals;
 /**
  * Created by Entelgy on 10/01/2015.
  */
 public class TxExecutiveMapperImplTest {
 
-    @Test()
-    public void testMapOuter() {
-        Executive executive = null;
+    private TxExecutiveMapperImpl txExecutiveMapperImpl;
+
+
+    @Before
+    public void init() {
+        txExecutiveMapperImpl = new TxExecutiveMapperImpl();
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testMapInOznr(DTOIntExecutivesFilter dtoIn) {
+
+        DTOIntExecutivesFilter dtoIntExecutivesFilter = new DTOIntExecutivesFilter();
+        dtoIntExecutivesFilter.setType("A");
+        dtoIntExecutivesFilter.setId("0100230404");
+
+        FormatoOZECNRE0 formato = new FormatoOZECNRE0();
+        formato = txExecutiveMapperImpl.mapInOznr(dtoIntExecutivesFilter);
+
+        assertEquals(dtoIntExecutivesFilter.getId(),formato.getIdusuar());
+    }
+
+    @Test
+    public void mapOutOznr() {
+        FormatoOZECNRS0 formatOutput = new FormatoOZECNRS0();
+        formatOutput.setEmailej("emailprueba@hotmail.com");
+        formatOutput.setIdejecu("0000002122");
+        formatOutput.setNomejec("Nombre ejecutivo 1");
+        formatOutput.setOfiejec("Oficina chile");
+        formatOutput.setTelejec("98234234");
 
         DTOIntExecutive dtoIntExecutive = new DTOIntExecutive();
-        dtoIntExecutive.setEmail("prueba@hotmail.com");
-        dtoIntExecutive.setExecutiveId("1");
-        dtoIntExecutive.setName("NombreEjecutivo");
-        Office office = new Office();
-        office.setName("oficina");
-        //dtoIntExecutive.setOffice(office);
-        dtoIntExecutive.setPhone("0102030405");
+        dtoIntExecutive = txExecutiveMapperImpl.mapOutOznr(formatOutput);
 
-        //executive = TxExecutiveMapperImpl.mapOuter(dtoIntExecutive);
-
-        assertEquals(dtoIntExecutive.getEmail(),executive.getEmail());
-        assertEquals(dtoIntExecutive.getName(),executive.getName());
-        assertEquals(dtoIntExecutive.getPhone(),executive.getPhone());
-        assertEquals(dtoIntExecutive.getOffice().getName(),executive.getOffice().getName());
+        assertEquals(formatOutput.getNomejec(),dtoIntExecutive.getName());
+        assertEquals(formatOutput.getTelejec(),dtoIntExecutive.getPhone());
+        assertEquals(formatOutput.getEmailej(),dtoIntExecutive.getEmail());
+        assertEquals(formatOutput.getOfiejec(),dtoIntExecutive.getOffice());
+        assertEquals(formatOutput.getIdejecu(),dtoIntExecutive.getExecutiveId());
     }
+
+
 
 }
