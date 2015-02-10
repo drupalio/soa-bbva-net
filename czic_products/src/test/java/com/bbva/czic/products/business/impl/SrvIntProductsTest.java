33f5d4c3e4911fd76a7c157f0788353ad8fe9197
danicorc <danicorc@gmail.com>
2015-02-10 11:51:07 -0500

    Test SrvIntProducts
    -listExtracts
    -getConditions

--- czic_products/src/test/java/com/bbva/czic/products/business/impl/SrvIntProductsTest.java
+++ czic_products/src/test/java/com/bbva/czic/products/business/impl/SrvIntProductsTest.java
@@ -3,50 +3,191 @@ package com.bbva.czic.products.business.impl;
 
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
+
+
+
+
+
+
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.fluttercode.datafactory.impl.DataFactory;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.MockitoAnnotations;
 
 import com.bbva.czic.products.business.ISrvIntProducts;
 import com.bbva.czic.products.business.dto.DTOIntConditions;
+import com.bbva.czic.products.business.dto.DTOIntExtract;
+import com.bbva.czic.products.business.dto.DTOIntExtractOutput;
+import com.bbva.czic.products.business.dto.DTOIntFilterExtract;
 import com.bbva.czic.products.business.dto.DTOIntProduct;
 import com.bbva.czic.products.dao.IProductsDAO;
+import com.bbva.czic.products.facade.v01.impl.SrvProductsV01;
+import com.bbva.czic.products.facade.v01.impl.SrvProductsV01Test;
 import com.bbva.jee.arq.spring.core.servicing.gce.BusinessServiceException;
 
 
 
 public class SrvIntProductsTest {
-
+	
+	final static DataFactory dataF = new DataFactory();
+	
 	@Mock
 	IProductsDAO productsDAO;
 
 	@InjectMocks
-	@Autowired
 	ISrvIntProducts srv;
+	
+	@Before
+	public void init(){
+		srv = new SrvIntProducts();
+        MockitoAnnotations.initMocks(this);
+	}
 
-	@Test()
-	public void testGetConditionsSucces() {
-		DTOIntConditions initialResult =null;
-
+//  ------------------ getConditions ------------------	
+	
+	@Test
+	public void testGetConditionsSuccess() {
+		
 		// setUp - expectation
-		DTOIntProduct dtoIntProduct = new DTOIntProduct();
-		when(productsDAO.getConditions(dtoIntProduct)).thenReturn(new DTOIntConditions());
+		DTOIntProduct dtoIntProduct = mockDTOIntProductEntity();
+		DTOIntConditions dtoIntCondition = SrvProductsV01Test.mockDTOIntConditionsEntity();
+		when(productsDAO.getConditions(any(DTOIntProduct.class))).thenReturn(dtoIntCondition);
 
-		initialResult= srv.getConditions(dtoIntProduct);
+		//Test
+		DTOIntConditions initialResult= srv.getConditions(dtoIntProduct);
+		assertNotNull(initialResult);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetConditionsNullInputDto() {
+		
+		// setUp - expectation
+		DTOIntProduct dtoIntProduct = null;
+		DTOIntConditions dtoIntCondition = SrvProductsV01Test.mockDTOIntConditionsEntity();
+		when(productsDAO.getConditions(any(DTOIntProduct.class))).thenReturn(dtoIntCondition);
 
+		//Test
+		DTOIntConditions initialResult= srv.getConditions(dtoIntProduct);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetConditionsNullOutputDto() {
+		
+		// setUp - expectation
+		DTOIntProduct dtoIntProduct = mockDTOIntProductEntity();
+		when(productsDAO.getConditions(any(DTOIntProduct.class))).thenReturn(null);
+
+		//Test
+		DTOIntConditions initialResult= srv.getConditions(dtoIntProduct);
+	}
+	
+//  ------------------ listExtracts ------------------	
+	
+	@Test
+	public void testListExtractsSuccess() {
+		
+		// setUp - expectation
+		DTOIntExtractOutput dtoExtractOutput = mockDTOIntExtractOutputEntity();
+		DTOIntFilterExtract dtoIntFilterExtract = mockDTOIntFilterExtractNoExtractIdEntity();
+		when(productsDAO.listExtracts(any(DTOIntFilterExtract.class))).thenReturn(dtoExtractOutput);
+
+		//Test
+		List<DTOIntExtract> initialResult= srv.listExtracts(dtoIntFilterExtract);
+		assertNotNull(initialResult);
+	}
+	
+	@Test
+	public void testGetExtractsSuccess() {
+		
+		// setUp - expectation
+		DTOIntExtractOutput dtoExtractOutput = mockDTOIntExtractOutputEntity();
+		DTOIntFilterExtract dtoIntFilterExtract = mockDTOIntFilterExtractEntity();
+		when(productsDAO.listExtracts(any(DTOIntFilterExtract.class))).thenReturn(dtoExtractOutput);
+
+		//Test
+		List<DTOIntExtract> initialResult= srv.listExtracts(dtoIntFilterExtract);
 		assertNotNull(initialResult);
 	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void testListExtractsNullInputDto() {
 
+		// setUp - expectation
+		DTOIntExtractOutput dtoExtractOutput = null;
+		DTOIntFilterExtract dtoIntFilterExtract = mockDTOIntFilterExtractNoExtractIdEntity();
+		when(productsDAO.listExtracts(any(DTOIntFilterExtract.class)))
+				.thenReturn(dtoExtractOutput);
 
+		// Test
+		List<DTOIntExtract> initialResult = srv
+				.listExtracts(dtoIntFilterExtract);
+		assertNotNull(initialResult);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testListExtractsNullOutputDto() {
 
-	private BusinessServiceException getBsnExeptionByAlias(String alias){
-		return new BusinessServiceException(alias);
+		// setUp - expectation
+		DTOIntFilterExtract dtoIntFilterExtract = mockDTOIntFilterExtractNoExtractIdEntity();
+		when(productsDAO.listExtracts(any(DTOIntFilterExtract.class)))
+				.thenReturn(null);
+
+		// Test
+		List<DTOIntExtract> initialResult = srv
+				.listExtracts(dtoIntFilterExtract);
+		assertNotNull(initialResult);
 	}
 
+	
+//  ------------------ Utils ------------------
+	
+	private DTOIntProduct mockDTOIntProductEntity() {
+		DTOIntProduct dto = new DTOIntProduct();
+		dto.setAlias(dataF.getRandomWord(7));
+		dto.setId(dataF.getNumberText(20));
+		dto.setName(dataF.getRandomWord(6));
+		dto.setOperable(true);
+		dto.setType("AC");
+		return dto;
+	}
+
+	private DTOIntExtractOutput mockDTOIntExtractOutputEntity() {
+		DTOIntExtractOutput dtoIntExtractOutput = new DTOIntExtractOutput();
+		dtoIntExtractOutput.setExtracts(new ArrayList<DTOIntExtract>());
+		return dtoIntExtractOutput;
+	}
+	
+	public DTOIntFilterExtract mockDTOIntFilterExtractNoExtractIdEntity() {
+		DTOIntFilterExtract filter = new DTOIntFilterExtract();
+		filter.setProductId(dataF.getNumberText(20));
+		filter.setEndMonth(dataF.getNumberBetween(10,12)+"");
+		filter.setStartMonth("01");
+		filter.setStartYear(dataF.getNumberBetween(2000,2005)+"");
+		filter.setEndYear(dataF.getNumberBetween(2010,2015)+"");
+		return filter;
+	}
+	
+	public DTOIntFilterExtract mockDTOIntFilterExtractEntity() {
+		DTOIntFilterExtract filter = new DTOIntFilterExtract();
+		filter.setProductId(dataF.getNumberText(20));
+		filter.setExtractId(dataF.getNumberText(8));
+		filter.setEndMonth("01");
+		filter.setStartMonth("01");
+		filter.setStartYear("2014");
+		filter.setEndYear("2014");
+		return filter;
+	}
 
 }
 
