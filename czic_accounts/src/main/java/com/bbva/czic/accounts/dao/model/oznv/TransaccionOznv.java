f7f31c3ebdb0e72216d1780c49bc32ac8eca5c71
Sebastian Gamba <sebas.gamba02@gmail.com>
2015-02-16 10:52:22 -0500

    modificacion del descriptor de oznv

--- czic_accounts/src/main/java/com/bbva/czic/accounts/dao/model/oznv/TransaccionOznv.java
+++ czic_accounts/src/main/java/com/bbva/czic/accounts/dao/model/oznv/TransaccionOznv.java
@@ -1,12 +1,11 @@
 package com.bbva.czic.accounts.dao.model.oznv;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.bbva.jee.arq.spring.core.host.ExcepcionTransaccion;
-import com.bbva.jee.arq.spring.core.host.InvocadorTransaccion;
 import com.bbva.jee.arq.spring.core.host.ServicioTransacciones;
+import com.bbva.jee.arq.spring.core.host.InvocadorTransaccion;
 
 /**
  * Invocador de la transacci&oacute;n <code>OZNV</code>
@@ -14,30 +13,26 @@ import com.bbva.jee.arq.spring.core.host.ServicioTransacciones;
  * @see PeticionTransaccionOznv
  * @see RespuestaTransaccionOznv
  */
-@Component(value = "transaccionOznv")
-@Profile(value="prod")
+@Component
 public class TransaccionOznv implements InvocadorTransaccion<PeticionTransaccionOznv,RespuestaTransaccionOznv> {
-
+	
 	@Autowired
 	private ServicioTransacciones servicioTransacciones;
-
+	
 	@Override
 	public RespuestaTransaccionOznv invocar(PeticionTransaccionOznv transaccion) throws ExcepcionTransaccion {
-		return servicioTransacciones
-				.invocar(PeticionTransaccionOznv.class, RespuestaTransaccionOznv.class, transaccion);
+		return servicioTransacciones.invocar(PeticionTransaccionOznv.class, RespuestaTransaccionOznv.class, transaccion);
 	}
-
+	
 	@Override
 	public RespuestaTransaccionOznv invocarCache(PeticionTransaccionOznv transaccion) throws ExcepcionTransaccion {
-		return servicioTransacciones
-				.invocar(PeticionTransaccionOznv.class, RespuestaTransaccionOznv.class, transaccion);
-	}
-
-	@Override
-	public void vaciarCache() {
+		return servicioTransacciones.invocar(PeticionTransaccionOznv.class, RespuestaTransaccionOznv.class, transaccion);
 	}
 
 	public void setServicioTransacciones(ServicioTransacciones servicioTransacciones) {
 		this.servicioTransacciones = servicioTransacciones;
 	}
+
+	@Override
+	public void vaciarCache() {}	
 }
