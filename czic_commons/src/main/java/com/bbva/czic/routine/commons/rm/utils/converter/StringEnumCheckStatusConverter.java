f7c5595be0a29c355a89ff971621c096af4428ac
Sebastian Gamba <sebas.gamba02@gmail.com>
2015-02-25 19:42:21 -0500

    Ajustes de numero de operacion en getMovement - llenar de ceros a la izq

--- czic_commons/src/main/java/com/bbva/czic/routine/commons/rm/utils/converter/StringEnumCheckStatusConverter.java
+++ czic_commons/src/main/java/com/bbva/czic/routine/commons/rm/utils/converter/StringEnumCheckStatusConverter.java
@@ -2,16 +2,26 @@ package com.bbva.czic.routine.commons.rm.utils.converter;
 
 import com.bbva.czic.dto.net.EnumCheckStatus;
 import com.bbva.czic.routine.mapper.CustomConverter;
+import com.bbva.czic.routine.mapper.converter.BidirectionalConverter;
 import com.bbva.czic.routine.mapper.metadata.Type;
 
 /**
  * @author Entelgy
  */
-public class StringEnumCheckStatusConverter extends CustomConverter<String, String> {
-
+public class StringEnumCheckStatusConverter extends BidirectionalConverter<String, String> {
 
 	@Override
-	public String convert(String source, Type<? extends String> destinationType) {
+	public String convertTo(String source, Type<String> destinationType) {
 		return EnumCheckStatus.getByCode(source).name();
+	}
+
+	@Override
+	public String convertFrom(String source, Type<String> destinationType) {
+		return null;
+	}
+
+	@Override
+	public boolean canConvert(Type<?> sourceType, Type<?> destinationType) {
+		return true;
 	}
 }
