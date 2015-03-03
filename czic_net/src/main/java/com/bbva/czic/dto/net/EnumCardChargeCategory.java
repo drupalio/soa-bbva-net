429bcabbfa9a09862794b37f9d890064cd7163f4
Julio Indriago <julio.indriago@entelgy.com>
2015-03-03 10:11:39 -0500

    FIX: mapea correctamente las categorias de las tarjetas.

--- czic_net/src/main/java/com/bbva/czic/dto/net/EnumCardChargeCategory.java
+++ czic_net/src/main/java/com/bbva/czic/dto/net/EnumCardChargeCategory.java
@@ -1,6 +1,7 @@
 package com.bbva.czic.dto.net;
 
 import java.io.Serializable;
+import java.util.regex.Pattern;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -12,12 +13,12 @@ import javax.xml.bind.annotation.XmlType;
 @XmlAccessorType(XmlAccessType.FIELD)
 public enum EnumCardChargeCategory implements Serializable {
 
-	OCIO("OCIO                               "), 
-	REGALOS_LIBROS_DISCOS("REGALOS, LIBROS, DISCOS            "), 
-	COMERCIOBASICO("COMERCIO BASICO				     "), 
-	ROPA_CALZADO_PERSONAL("ROPA, CALZADO PERSONAL             "), 
-	VARIOS("VARIOS							 "), 
-	COMPRASPORCANALES("COMPRAS POR CANALES				 ")
+	OCIO("OCIO"),
+	REGALOS_LIBROS_DISCOS("(REGALOS|LIBROS,DISCOS)"),
+	COMERCIOBASICO("COMERCIO BASICO"),
+	ROPA_CALZADO_PERSONAL("(ROPA|CALZADO Y PERSONAL)"),
+	VARIOS("VARIOS"),
+	COMPRASPORCANALES("COMPRAS POR CANALES")
 	;
 
 	private String text;
@@ -33,7 +34,7 @@ public enum EnumCardChargeCategory implements Serializable {
 	public static EnumCardChargeCategory fromString(String text) {
 	    if (text != null) {
 	      for (EnumCardChargeCategory cardChargeCategory : EnumCardChargeCategory.values()) {
-	        if (text.equalsIgnoreCase(cardChargeCategory.text)) {
+	        if (Pattern.compile(cardChargeCategory.text).matcher(text).matches()) {
 	          return cardChargeCategory;
 	        }
 	      }
